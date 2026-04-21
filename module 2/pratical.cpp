bb30508267cc8776d82ac7f1a623bc638f88898e
manthanrana2510@gamil.com <manthanrana2510@gmail.com>
2026-04-21 17:00:32 +0530

    module2.c

---
+++ module 2/pratical.cpp
@@ -0,0 +1,11 @@
+# include <stdio.h> 
+main (){
+	char ch='m'; //char=>1bycte %ch
+	int x=100; //int=>4bytes %d
+	float pi=3.14; //float 4bytes %f
+	printf ("\n char=%c",ch);
+	printf("\n x=%d",x);
+	printf("\n pi=%f",pi);
+}
+	
+	
