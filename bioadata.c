5784fef5c1d6fd117b8766093489136cd398d6ec
manthanrana2510-max <manthanrana2510@gmail.com>
2026-05-11 16:11:42 +0530

    Add files via upload

---
+++ bioadata.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+void main(){
+	char name[20],address[500],email[50000];
+	int phoneno,age;
+	printf("enter name ");
+	sacnf("%s",&name);
+	printf("enter address ");
+	sacnf("%s",&address);
+	printf("enter eamil ");
+	sacnf("%s",&email);
+	
+	printf("enter phoneno ");
+	sacnf("%d",&name);
+	printf("enter age ");
+	sacnf("%d",&age);
+	
+}
+
---
+++ str2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main(){
+	char name[20];
+	printf("\n enter name");
+	get(name);
+	printf("\n name =%s",name);
+	return 0;
+}
